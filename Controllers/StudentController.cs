ac597a5dec6b2141156427e709087ca8d571b689
SarayutBz <sarayut.aiamaurai@gmail.com>
2024-12-26 13:22:53 +0700

    จบแล้ว

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,40 +1,92 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Security.Cryptography;
 using WebApplication2.Data;
 using WebApplication2.Models;
 
-namespace WebApplication2.Controllers
+public class StudentController : Controller
 {
-    public class StudentController : Controller
+    private readonly ApplicationDBContext _db;
+    public StudentController(ApplicationDBContext db)
     {
-        private readonly ApplicationDBContext _db;
-        public StudentController(ApplicationDBContext db)
-        {
-            _db = db;
-        }
+        _db = db;
+    }
 
-        public IActionResult Index()
-        {
-            IEnumerable <Student> allStudent = _db.Students;
+    public IActionResult Index()
+    {
+        IEnumerable<Student> allStudent = _db.Students;
+        return View(allStudent);
+    }
 
+    // GET Method for Create
+    public IActionResult Create()
+    {
+        return View();
+    }
 
-            return View(allStudent);
-        }
-        // GET METHOD
-        public IActionResult Create()
-        {
-            return View();
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        //POST
-        public IActionResult Create(Student obj)
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    // POST Method for Create
+    public IActionResult Create(Student obj)
+    {
+        if (ModelState.IsValid)
         {
             _db.Students.Add(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
-
-
+        return View(obj);
     }
+
+    // GET Method for Edit
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        var obj = _db.Students.Find(id);
+        if (obj == null)
+        {
+            return NotFound();
+        }
+        return View(obj);
+    }
+
+    // POST Method for Edit
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Student obj)
+    {
+        if (ModelState.IsValid)
+        {
+            _db.Students.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+    }
+
+
+    // GET Method Delete
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        var obj = _db.Students.Find(id);
+        if (obj == null)
+        {
+            return NotFound();
+        }
+        _db.Students.Remove(obj);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+
+
+
+
 }
--- Models/Student.cs
+++ Models/Student.cs
@@ -7,11 +7,11 @@ namespace WebApplication2.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage ="กรุณาป้อนชื่อนักเรียน")]
         [DisplayName("ชื่อนักเรียน")]
         public string? Name { get; set; }
         [DisplayName("คะแนนสอบ")]
-        [Range(0,100)]
+        [Range(0,100,ErrorMessage ="กรุณาป้อนคะแนนในช่วง 0-100")]
         public int Score { get; set; }
     }
 }
